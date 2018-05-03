75383da037c0e820908efd40cfdfa4526d13521a
Ryan O'Kane <ryan.okane8@gmail.com>
2018-05-03 14:33:50 +1000

    enhanced Autocompleter functionality to include payload with suggestions

--- redisearch/autocomplete.go
+++ redisearch/autocomplete.go
@@ -40,7 +40,13 @@ func (a *Autocompleter) AddTerms(terms ...Suggestion) error {
 
 	i := 0
 	for _, term := range terms {
-		if err := conn.Send("FT.SUGADD", a.name, term.Term, term.Score); err != nil {
+
+		args := redis.Args{a.name, term.Term, term.Score}
+		if payload := term.Payload; payload != "" {
+			args = append(args, "PAYLOAD", payload)
+		}
+
+		if err := conn.Send("FT.SUGADD", args...); err != nil {
 			return err
 		}
 		i++
@@ -60,27 +66,38 @@ func (a *Autocompleter) AddTerms(terms ...Suggestion) error {
 // Suggest gets completion suggestions from the Autocompleter dictionary to the given prefix.
 // If fuzzy is set, we also complete for prefixes that are in 1 Levenshten distance from the
 // given prefix
-func (a *Autocompleter) Suggest(prefix string, num int, fuzzy bool) ([]Suggestion, error) {
+func (a *Autocompleter) Suggest(prefix string, num int, fuzzy, withPayloads bool) ([]Suggestion, error) {
 	conn := a.pool.Get()
 	defer conn.Close()
+
+	inc := 2
 
 	args := redis.Args{a.name, prefix, "MAX", num, "WITHSCORES"}
 	if fuzzy {
 		args = append(args, "FUZZY")
+	}
+	if withPayloads {
+		args = append(args, "WITHPAYLOADS")
+		inc = 3
 	}
 	vals, err := redis.Strings(conn.Do("FT.SUGGET", args...))
 	if err != nil {
 		return nil, err
 	}
 
-	ret := make([]Suggestion, 0, len(vals)/2)
-	for i := 0; i < len(vals); i += 2 {
+	ret := make([]Suggestion, 0, len(vals)/inc)
+	for i := 0; i < len(vals); i += inc {
 
 		score, err := strconv.ParseFloat(vals[i+1], 64)
 		if err != nil {
 			continue
 		}
-		ret = append(ret, Suggestion{Term: vals[i], Score: score})
+
+		suggestion := Suggestion{Term: vals[i], Score: score}
+		if withPayloads {
+			suggestion.Payload = vals[i+2]
+		}
+		ret = append(ret, suggestion)
 
 	}
 
