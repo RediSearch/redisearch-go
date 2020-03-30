5e45a0a5b690c5caaf65fe2dfafaa3ad65d9e2b5
filipecosta90 <filipecosta.90@gmail.com>
2020-03-30 15:36:01 +0100

    [add] extended coverage for document.go

--- redisearch/document_test.go
+++ redisearch/document_test.go
@@ -2,6 +2,7 @@ package redisearch_test
 
 import (
 	"github.com/RediSearch/redisearch-go/redisearch"
+	"reflect"
 	"testing"
 )
 
@@ -25,6 +26,79 @@ func TestEscapeTextFileString(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := redisearch.EscapeTextFileString(tt.args.value); got != tt.want {
 				t.Errorf("EscapeTextFileString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDocument_EstimateSize(t *testing.T) {
+	type fields struct {
+		Id         string
+		Score      float32
+		Payload    []byte
+		Properties map[string]interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		wantSz int
+	}{
+		{
+			"only-id", fields{"doc1", 1.0, []byte{}, map[string]interface{}{},}, len("doc1"),
+		},
+		{
+			"id-payload", fields{"doc1", 1.0, []byte("payload"), map[string]interface{}{},}, len("doc1") + len([]byte("payload")),
+		},
+		{
+			"id-payload-fields", fields{"doc1", 1.0, []byte("payload"), map[string]interface{}{"text1": []byte("text1")},}, len("doc1") + len([]byte("payload")) + 2*len([]byte("text1")),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &redisearch.Document{
+				Id:         tt.fields.Id,
+				Score:      tt.fields.Score,
+				Payload:    tt.fields.Payload,
+				Properties: tt.fields.Properties,
+			}
+			if gotSz := d.EstimateSize(); !reflect.DeepEqual(gotSz, tt.wantSz) {
+				t.Errorf("EstimateSize() = %v, want %v", gotSz, tt.wantSz)
+			}
+		})
+	}
+}
+
+func TestDocument_SetPayload(t *testing.T) {
+	type fields struct {
+		Id         string
+		Score      float32
+		Payload    []byte
+		Properties map[string]interface{}
+	}
+	type args struct {
+		payload []byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		wantPayload []byte
+	}{
+		{"empty-payload", fields{"doc1", 1.0, []byte{}, map[string]interface{}{},}, args{[]byte{}}, []byte{}},
+		{"simple-set", fields{"doc1", 1.0, []byte{}, map[string]interface{}{},}, args{[]byte("payload")},[]byte("payload")},
+		{"set-with-previous-payload", fields{"doc1", 1.0, []byte("previous_payload"), map[string]interface{}{},}, args{[]byte("payload")},[]byte("payload")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &redisearch.Document{
+				Id:         tt.fields.Id,
+				Score:      tt.fields.Score,
+				Payload:    tt.fields.Payload,
+				Properties: tt.fields.Properties,
+			}
+			d.SetPayload(tt.args.payload)
+			if !reflect.DeepEqual(d.Payload, tt.wantPayload) {
+				t.Errorf("SetPayload() = %v, want %v", d.Payload, tt.wantPayload)
 			}
 		})
 	}
