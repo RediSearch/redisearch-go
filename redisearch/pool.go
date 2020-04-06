c3a2be0c9442aa9e65c28969af560ea1eda1eac8
filipecosta90 <filipecosta.90@gmail.com>
2020-04-06 13:53:28 +0100

    [add] swapped deprecated redis.NewPool to redis.Pool constructor. preserving all errors on multihost pool CLose()

--- redisearch/pool.go
+++ redisearch/pool.go
@@ -1,6 +1,7 @@
 package redisearch
 
 import (
+	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"math/rand"
 	"sync"
@@ -17,16 +18,16 @@ type SingleHostPool struct {
 }
 
 func NewSingleHostPool(host string) *SingleHostPool {
-	ret := &redis.Pool{Dial: func() (redis.Conn, error) {
+	pool := &redis.Pool{Dial: func() (redis.Conn, error) {
 		return redis.Dial("tcp", host)
 	}, MaxIdle: maxConns}
-	ret.TestOnBorrow = func(c redis.Conn, t time.Time) (err error) {
+	pool.TestOnBorrow = func(c redis.Conn, t time.Time) (err error) {
 		if time.Since(t) > time.Second {
 			_, err = c.Do("PING")
 		}
 		return err
 	}
-	return &SingleHostPool{ret}
+	return &SingleHostPool{pool}
 }
 
 type MultiHostPool struct {
@@ -49,10 +50,9 @@ func (p *MultiHostPool) Get() redis.Conn {
 	host := p.hosts[rand.Intn(len(p.hosts))]
 	pool, found := p.pools[host]
 	if !found {
-		pool = redis.NewPool(func() (redis.Conn, error) {
-			// TODO: Add timeouts. and 2 separate pools for indexing and querying, with different timeouts
+		pool := &redis.Pool{Dial: func() (redis.Conn, error) {
 			return redis.Dial("tcp", host)
-		}, maxConns)
+		}, MaxIdle: maxConns}
 		pool.TestOnBorrow = func(c redis.Conn, t time.Time) (err error) {
 			if time.Since(t) > time.Second {
 				_, err = c.Do("PING")
@@ -69,11 +69,15 @@ func (p *MultiHostPool) Get() redis.Conn {
 func (p *MultiHostPool) Close() (err error) {
 	p.Lock()
 	defer p.Unlock()
-	for _, pool := range p.pools {
+	for host, pool := range p.pools {
 		poolErr := pool.Close()
 		//preserve pool error if not nil but continue
 		if poolErr != nil {
-			err = poolErr
+			if err == nil {
+				err = fmt.Errorf("Error closing pool for host %s. Got %v.", host, poolErr)
+			} else {
+				err = fmt.Errorf("%v Error closing pool for host %s. Got %v.", err, host, poolErr)
+			}
 		}
 	}
 	return
--- redisearch/pool_test.go
+++ redisearch/pool_test.go
@@ -1,6 +1,7 @@
 package redisearch
 
 import (
+	"github.com/gomodule/redigo/redis"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -34,6 +35,7 @@ func TestNewMultiHostPool(t *testing.T) {
 
 func TestMultiHostPool_Close(t *testing.T) {
 	host, password := getTestConnectionDetails()
+	// Test a simple flow
 	if password == "" {
 		oneMulti := NewMultiHostPool([]string{host,})
 		conn := oneMulti.Get()
@@ -47,5 +49,48 @@ func TestMultiHostPool_Close(t *testing.T) {
 		assert.NotNil(t, connMulti)
 		err = severalMulti.Close()
 		assert.Nil(t, err)
+	}
+	// Exhaustive test
+	dial := func() (redis.Conn, error) {
+		return redis.Dial("tcp", host, redis.DialPassword(password))
+	}
+	pool1 := &redis.Pool{Dial: dial, MaxIdle: maxConns}
+	pool2 := &redis.Pool{Dial: dial, MaxIdle: maxConns}
+	pool3 := &redis.Pool{Dial: dial, MaxIdle: maxConns}
+	//Close pull3 prior to enforce error
+	pool3.Close()
+	pool4 := &redis.Pool{Dial: dial, MaxIdle: maxConns}
+
+	type fields struct {
+		pools map[string]*redis.Pool
+		hosts []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{"empty", fields{map[string]*redis.Pool{}, []string{}}, false,},
+		{"normal", fields{map[string]*redis.Pool{"hostpool1": pool1}, []string{"hostpool1"}}, false,},
+		{"pool3-already-close", fields{map[string]*redis.Pool{"hostpool2": pool2, "hostpool3": pool3, "hostpool4": pool4,}, []string{"hostpool2", "hostpool3", "hostpool3"}}, false,},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &MultiHostPool{
+				pools: tt.fields.pools,
+				hosts: tt.fields.hosts,
+			}
+			if err := p.Close(); (err != nil) != tt.wantErr {
+				t.Errorf("Close() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			// ensure all connections are really closed
+			if !tt.wantErr {
+				for _, pool := range p.pools {
+					if _, err := pool.Get().Do("PING"); err == nil {
+						t.Errorf("expected error after connection closed. Got %v", err)
+					}
+				}
+			}
+		})
 	}
 }
