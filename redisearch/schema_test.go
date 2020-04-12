7b0f8aca7a691e009a604348ad80280c316deedf
filipecosta90 <filipecosta.90@gmail.com>
2020-04-12 17:02:05 +0100

    [add] added test for SerializeSchema

--- redisearch/schema_test.go
+++ redisearch/schema_test.go
@@ -1,9 +1,10 @@
 package redisearch
 
 import (
+	"github.com/gomodule/redigo/redis"
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
-	"github.com/gomodule/redigo/redis"
 )
 
 func TestNewSchema(t *testing.T) {
@@ -42,7 +43,16 @@ func TestSerializeSchema(t *testing.T) {
 		want    redis.Args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"default-args", args{NewSchema(DefaultOptions), redis.Args{}}, redis.Args{"SCHEMA"}, false},
+		{"no-frequencies", args{NewSchema(Options{NoFrequencies: true}), redis.Args{}}, redis.Args{"NOFREQS", "SCHEMA"}, false},
+		{"no-offsets", args{NewSchema(Options{NoOffsetVectors: true}), redis.Args{}}, redis.Args{"NOOFFSETS", "SCHEMA"}, false},
+		{"default-and-numeric", args{NewSchema(DefaultOptions).AddField(NewNumericField("numeric-field")), redis.Args{}}, redis.Args{"SCHEMA", "numeric-field", "NUMERIC"}, false},
+		{"default-and-numeric-sortable", args{NewSchema(DefaultOptions).AddField(NewSortableNumericField("numeric-field")), redis.Args{}}, redis.Args{"SCHEMA", "numeric-field", "NUMERIC", "SORTABLE"}, false},
+		{"default-and-text", args{NewSchema(DefaultOptions).AddField(NewTextField("text-field")), redis.Args{}}, redis.Args{"SCHEMA", "text-field", "TEXT"}, false},
+		{"default-and-text-with-options", args{NewSchema(DefaultOptions).AddField(NewTextFieldOptions("text-field", TextFieldOptions{Weight: 5.0, Sortable: true, NoStem: false, NoIndex: false})), redis.Args{}}, redis.Args{"SCHEMA", "text-field", "TEXT", "WEIGHT", float32(5.0), "SORTABLE"}, false},
+		{"default-and-tag", args{NewSchema(DefaultOptions).AddField(NewTagField("tag-field")), redis.Args{}}, redis.Args{"SCHEMA", "tag-field", "TAG", "SEPARATOR",","}, false},
+		{"default-and-tag-with-options", args{NewSchema(DefaultOptions).AddField(NewTagFieldOptions("tag-field", TagFieldOptions{Sortable: true, NoIndex: false,Separator:byte(',')})), redis.Args{}}, redis.Args{"SCHEMA", "tag-field", "TAG", "SEPARATOR",",","SORTABLE"}, false},
+
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -54,6 +64,7 @@ func TestSerializeSchema(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("SerializeSchema() got = %v, want %v", got, tt.want)
 			}
+			assert.Equal(t, got, tt.want)
 		})
 	}
 }
