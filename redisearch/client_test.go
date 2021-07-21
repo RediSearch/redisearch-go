a9d341d869e5af27abf4b588becd3a636a5ad3af
AvitalFineRedis <avital.fine@redislabs.com>
2021-07-21 10:53:35 +0300

    remove duplicated test

--- redisearch/client_test.go
+++ redisearch/client_test.go
@@ -988,55 +988,6 @@ func TestClient_CreateJsonIndex(t *testing.T) {
 	assert.Equal(t, "John", docs[1].Properties["name"])
 }
 
-func TestClient_CreateJsonIndex(t *testing.T) {
-	c := createClient("create-json-index")
-	flush(c)
-	version, _ := c.getRediSearchVersion()
-	if version < 20200 {
-		// IndexDefinition is available for RediSearch 2.0+
-		return
-	}
-
-	// Create a schema
-	schema := NewSchema(DefaultOptions).
-		AddField(NewTextFieldOptions("$.name", TextFieldOptions{Sortable: true, PhoneticMatcher: PhoneticDoubleMetaphoneEnglish, As: "name"})).
-		AddField(NewNumericFieldOptions("$.age", NumericFieldOptions{As: "age"}))
-
-	// IndexDefinition is available for RediSearch 2.0+
-	// In this example we will only index keys started by product:
-	indexDefinition := NewIndexDefinition().SetIndexOn(JSON).AddPrefix("create-json-index:")
-
-	// Add the Index Definition
-	err := c.CreateIndexWithIndexDefinition(schema, indexDefinition)
-	assert.Nil(t, err)
-
-	// Create docs with a name that has the same phonetic matcher
-	vanillaConnection := c.pool.Get()
-	_, err = vanillaConnection.Do("JSON.SET", "create-json-index:doc1", "$", "{\"name\":\"Jon\", \"age\": 25}")
-	assert.Nil(t, err)
-	_, err = vanillaConnection.Do("JSON.SET", "create-json-index:doc2", "$", "{\"name\":\"John\", \"age\": 25}")
-	assert.Nil(t, err)
-
-	// Wait for all documents to be indexed
-	info, err := c.Info()
-	assert.Nil(t, err)
-	for info.IsIndexing {
-		time.Sleep(time.Second)
-		info, _ = c.Info()
-	}
-
-	assert.Equal(t, uint64(2), info.DocCount)
-	assert.Equal(t, false, info.IsIndexing)
-	assert.Equal(t, uint64(0), info.HashIndexingFailures)
-	docs, total, err := c.Search(NewQuery("Jon").
-		SetReturnFields("name"))
-	assert.Nil(t, err)
-	// Verify that the we've received 2 documents ( Jon and John )
-	assert.Equal(t, 2, total)
-	assert.Equal(t, "\"Jon\"", docs[0].Properties["name"])
-	assert.Equal(t, "\"John\"", docs[1].Properties["name"])
-}
-
 func TestClient_CreateIndex_failure(t *testing.T) {
 	c := createClient("create-index-failure")
 	flush(c)
